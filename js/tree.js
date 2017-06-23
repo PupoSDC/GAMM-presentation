7d5cc6763d191bfecdbebb36897ce837f2c8312b
pupoSDC <pupo163@hotmail.com>
2017-06-23 15:26:28 +0200

    fuck canvas...

--- js/tree.js
+++ js/tree.js
@@ -8,6 +8,7 @@ var tree = {
     nodes: [],
     canvas: null,
     context: null,
+
     initialise: function(canvas,number_of_levels,percentage){
         
         this.number_of_levels    = number_of_levels;
@@ -18,6 +19,7 @@ var tree = {
         this.context.strokeStyle = '#17253C';
         this.context.font        = "bold 20px Arial";
         this.context.lineWidth = 2;
+        
         var k = 0;
         for( var i = 0; i <= this.number_of_levels; i++ ){
             var nodes_to_draw = Math.pow(2,i);
@@ -26,21 +28,31 @@ var tree = {
                 var node =  {
                     x: this.canvas.width  * (i+1)   / (this.number_of_levels+2) * this.percentage,
                     y: this.canvas.height * ( (j+1) / (nodes_to_draw+1) ),
-                    i: k 
+                    i: k,
+                    r: 20,
+                    selected: false
                 };
                 this.nodes.push(node);
                 k++;
             }
-        }    
+        }  
+
+        this.canvas.onclick  = this.onclick();
+
+        //window.onresize =  function(){ 
+        //    //this.update();
+        //    this.canvas.width  = this.canvas.getBoundingClientRect().width;
+        //    this.canvas.height = this.canvas.getBoundingClientRect().height;
+        //}
     },
 
     drawCircle: function(node){
         this.context.beginPath();
-        this.context.arc( node.x, node.y, 20, 0, 2 * Math.PI, false);  
+        this.context.arc( node.x, node.y, node.r, 0, 2 * Math.PI, false);  
         this.context.lineWidth = 2;
         this.context.stroke();
         var temp = this.context.fillStyle;
-        this.context.fillStyle   = 'white';
+        this.context.fillStyle    = node.selected ? '#009DE0' : 'white';
         this.context.fill();
         this.context.fillStyle   = temp;
         this.context.fillText(
@@ -50,6 +62,7 @@ var tree = {
         );
         //context.addHitRegion({id: "node_" + i });
     },
+
 
     drawLine: function(x1,y1,x2,y2){
         this.context.beginPath();
@@ -102,7 +115,37 @@ var tree = {
 
         this.drawDomain();
 
+    },
+
+    onclick: function() {
+
+        var nodes  = this.nodes
+      
+
+        return function(e){
+
+            var rect = this.getBoundingClientRect();   
+
+            var x = (e.clientX - rect.left );   
+            var y = (e.clientY - rect.top  );  
+
+            console.log( x+ " " + y )
+
+
+            for( var i = 0; i < 1; i++ ){
+
+                var dx   = x - nodes[i].x;
+                var dy   = y - nodes[i].y;
+                var dist = dx * dx + dy * dy;
+                
+                if( dist <= nodes[i].r * nodes[i].r ){
+                    console.log(nodes[i])
+                }
+            }  
+
+        };
     }
+
 }
 
 tree.initialise("myCanvas",4,0.8);
