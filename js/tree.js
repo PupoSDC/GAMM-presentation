463d7bd4729b0574e98cf813ddefdb7fb04b1375
pupo162 <pupo163@hotmail.com>
2017-06-24 14:35:34 +0200

    finished tree, missing rules!

--- js/tree.js
+++ js/tree.js
@@ -1,6 +1,7 @@
 
 var tree = {
 
+    container:        null,
     snap:             null,
     number_of_levels: null,
     domain:           [],
@@ -12,8 +13,8 @@ var tree = {
     clickedNode:      null,
 
     initialise: function(id,number_of_levels,percentage,values){
-        
-        this.snap              = Snap(id);
+        this.container         = document.getElementById(id);
+        this.snap              = Snap('#' + id);
         this.number_of_levels  = number_of_levels;
         this.dadnode = {
             x        : this.width  / (this.number_of_levels + 2),
@@ -29,8 +30,8 @@ var tree = {
         this.dadnode.left  = this.makeNode(this.dadnode,1,0);
         this.dadnode.right = this.makeNode(this.dadnode,1,1);
         
-        this.printLegs(this.dadnode);
-        this.printNode(this.dadnode);
+        this.printTree(this.dadnode);
+
         this.adjustPlots(values);
     },
 
@@ -48,8 +49,7 @@ var tree = {
             parent   : parent,
             left     : null,
             right    : null,
-            leftleg  : null,
-            rightleg : null,
+            group    : null,
             id       : parent.id + right.toString(),
             value    : null
         }
@@ -60,33 +60,50 @@ var tree = {
         return node;
     },
 
-    printNode(node){
+    printTree(node){
+
         if( node == null ){ return; }
 
+        node.group = this.snap.group();
+
+        if(node.left != null || node.right != null){
+            // Draw the subtrees recursively and add them to a single group
+            var line_1 = this.snap.line(node.x,node.y,node.left.x,node.left.y);
+            var line_2 = this.snap.line(node.x,node.y,node.right.x,node.right.y);
+            line_1.attr({ stroke: "#17253C", });
+            line_2.attr({ stroke: "#17253C", });
+            line_1.appendTo( node.group );
+            line_2.appendTo( node.group );
+            this.printTree(node.left).appendTo( node.group );
+            this.printTree(node.right).appendTo( node.group );
+        } else {
+
+            var n_values = Math.pow(2,this.number_of_levels);
+            node.value = this.snap.rect( 
+                this.width * 0.7,
+                this.domain.length * this.height / n_values,
+                this.width * 0.3,
+                this.height / n_values
+            ).attr({
+                fill:       "#009DE0",
+                stroke:     '#17253C',
+                strokeWidth: 3
+            });
+
+            this.domain.push(node.value);
+            node.value.appendTo( node.group );
+        }
+
+
         node.form = this.snap.circle( node.x, node.y, node.r );
+        node.form.appendTo( node.group );
+ 
         node.form.attr({
             fill:       "white",
             stroke:     '#009DE0',
             strokeWidth: 3,
             id:          node.id
         });
-
-        if( node.left == null && node.right == null){
-            var n_values = Math.pow(2,this.number_of_levels);
-            var rect = this.snap.rect( 
-                this.width * 0.7,
-                this.domain.length * this.height / n_values,
-                this.width * 0.3,
-                this.height / n_values
-            );
-            rect.attr({
-                fill:       "#009DE0",
-                stroke:     '#17253C',
-                strokeWidth: 3
-            });
-            this.domain.push(rect);
-            node.value = rect;
-        }
 
         var self = this;
         if( node.parent != null ){
@@ -95,20 +112,7 @@ var tree = {
             });
         }
 
-        this.printNode(node.left);
-        this.printNode(node.right);
-    },
-
-    printLegs(node){
-        if( node.left == null || node.right == null ){
-            return;
-        }
-        node.leftleg  = this.snap.line(node.x,node.y,node.left.x,node.left.y);
-        node.rightleg = this.snap.line(node.x,node.y,node.right.x,node.right.y);
-        node.leftleg.attr({ stroke: "#17253C", });
-        node.rightleg.attr({ stroke: "#17253C", });
-        this.printLegs(node.left);
-        this.printLegs(node.right);
+        return node.group;
     },
 
     nodeClicked(node){
@@ -126,48 +130,82 @@ var tree = {
         }
     },
 
-    groupAllChildren(node,group){
-        if( group == null ){ group = this.snap.group(); }
-
-        if( node.left == null || node.right == null ){
-            node.value.appendTo(group);
-            return group;  
+    getCenterOfNode(node){
+        var box      = node.getBoundingClientRect();
+        var outerbox = this.container.getBoundingClientRect();
+        var scalex = this.width  / outerbox.width;
+        var scaley = this.height / outerbox.height;
+        return {
+            x: ( box.left + box.width  / 2 ) * scalex,
+            y: ( box.top  + box.height / 2 ) * scaley
         }
-
-        group = this.groupAllChildren(node.left,group);
-        group = this.groupAllChildren(node.right,group);
-
-        node.leftleg.appendTo( group );
-        node.rightleg.appendTo( group );
-
-        node.left.form.appendTo( group );
-        node.right.form.appendTo( group );
-
-        return group;
     },
 
     moveTwoBranches(gp1,gp2){
-  
-        var dy = gp1.form.getBBox().y - gp2.form.getBBox().y;
-        var dx = gp1.form.getBBox().x - gp2.form.getBBox().x;
-        if( dx != 0.0 ){ 
+        var c1 = this.getCenterOfNode(gp1.form.node);
+        var c2 = this.getCenterOfNode(gp2.form.node);
+
+        if( c1.x != c2.x || c1.y == c2.y ){ 
             gp1.form.attr({ stroke:  '#009DE0' });
             gp2.form.attr({ stroke:  '#009DE0' });
             return; 
         }
+        var t1 = gp1.group.transform().localMatrix.f;
+        var t2 = gp2.group.transform().localMatrix.f;
+        var v1 = this.getMovedValues(gp1);
+        var v2 = this.getMovedValues(gp2);
 
-        var g1 = this.groupAllChildren( gp1 ); gp1.form.appendTo( g1 ); 
-        var g2 = this.groupAllChildren( gp2 ); gp2.form.appendTo( g2 ); 
+        mixNode = this.mixNode;
 
-        g1.animate({ transform: 't0,' + ( -1.0 * dy ) }, 1000, function(){
-            g1.transform('t0,0'); 
-            gp1.form.attr({ stroke:  '#009DE0' });
+        gp1.group.animate({ 
+            transform: 't0,' + (c2.y - c1.y + t1 )  
+            }, 1000, function(){
+                gp1.form.attr({ stroke:  '#009DE0' });
+                gp1.group.transform('t0,0');
+
+                for( var i = 0; i < v1.length; i++ ){
+                    var temp = v1[i].node.width.baseVal.value;
+                    v1[i].node.width.baseVal.value = v2[i].node.width.baseVal.value
+                    v2[i].node.width.baseVal.value = temp
+                }
+
+                if( v1.length == 1 ){
+                    mixNode(gp1.parent);
+                    mixNode(gp2.parent);
+                    return;
+                }
+
         });
-
-        g2.animate({ transform: 't0,' + (  1.0 * dy ) }, 1000, function(){  
-            g2.transform('t0,0');
-            gp2.form.attr({ stroke:  '#009DE0' });
+        gp2.group.animate({ 
+            transform: 't0,' + (c1.y - c2.y + t2) 
+            }, 1000, function(){
+                gp2.form.attr({ stroke:  '#009DE0' });
+                gp2.group.transform('t0,0');
         });
+        
+
+    },
+
+    getMovedValues: function(node,values){
+        if( values == null ){ values = []; }
+        if( node.value == null){
+            values = this.getMovedValues(node.left,values);
+            values = this.getMovedValues(node.right,values);
+        } else {
+            values.push( node.value );
+        }
+        return values;
+    },
+
+    mixNode(node){
+        if( node.left.value == null || node.right.value == null ){
+            return;
+        }
+        var temp = (  node.left.value.node.width.baseVal.value + 
+                      node.right.value.node.width.baseVal.value  ) / 2;
+        node.left.value.node.width.baseVal.value = temp;
+        node.right.value.node.width.baseVal.value = temp;
+        return;
     },
 
     adjustPlots(values){
@@ -175,11 +213,7 @@ var tree = {
             this.domain[i].node.width.baseVal.value = tree.width * 0.3 * values[i];
         }
     },
-
-    linkPlotAndHips(){
-
-    }
 }
 
-tree.initialise("#svgOne",4,0.8,[0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]);
+tree.initialise("svgOne",4,0.8,[0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]);
 
